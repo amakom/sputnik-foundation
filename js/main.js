0f2c7594940a7ea869b9344abd4723ebc6113db4
amakom <obiajuluamakom@gmail.com>
2021-12-14 15:06:18 +0100

    Update Theme Colors

--- js/main.js
+++ js/main.js
@@ -27,6 +27,35 @@ function styleSwitcherToggle(){
 
     styleSwitcherToggler.addEventListener("click", function(){
         styleSwitcher.classList.toggle("open");
+        this.querySelector("i").classList.toggle("fa-times");
+        this.querySelector("i").classList.toggle("fa-cog");
     });
 }
 styleSwitcherToggle();
+
+/*---------------------------------
+theme colors
+-----------------------------------*/
+function themeColors(){
+    const colorStyle = document.querySelector(".js-color-style"),
+    themeColorsContainer = document.querySelector(".js-theme-colors");
+
+    themeColorsContainer.addEventListener("click", ({target}) => {
+        if(target.classList.contains("js-theme-color-item")){
+            localStorage.setItem("color", target.getAttribute("data-js-theme-color"));
+            setColor();
+        }
+    });
+
+    function setColor(){
+        let path = colorStyle.getAttribute("href").split("/");
+        path = path.slice(0, path.length-1);
+        colorStyle.setAttribute("href", path.join("/") + "/" + localStorage.getItem("color") + ".css");
+
+        if(document.querySelector(".js-theme-color-item.active")){
+            document.querySelector(".js-theme-color-item.active").classList.remove("active");
+        }
+        document.querySelector("[data-js-theme-color=" + localStorage.getItem("color") + "]").classList.add("active");
+    }
+}
+themeColors();
