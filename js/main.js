9318ef86790c9b37f4605453ef83ad498c3c698d
amakom <obiajuluamakom@gmail.com>
2021-12-14 04:16:50 +0100

    Foundation first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+
+
+/*---------------------------------
+testimonial slider
+-----------------------------------*/
+function testimonialSlider(){
+    const carouselOne = document.getElementById('carouselOne');
+    if(carouselOne){ /* if the element  exists*/
+        carouselOne.addEventListener('slid.bs.carousel', function () {
+            const activeItem = this.querySelector(".active");
+            document.querySelector(".js-testimonial-img").src = 
+            activeItem.getAttribute("data-js-testimonial-img");
+        })
+    }
+}
+testimonialSlider();
